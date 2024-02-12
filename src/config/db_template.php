8166d3e5811be7accf7f7f92786d45908126d1d1
Jean Alves de Oliveira <jeanalvesoliveira@gmail.com>
2024-02-12 00:32:35 -0300

    removendo o arquivo de configurações com o banco de dados do repositório e adicionando um template

---
+++ src/config/db_template.php
@@ -0,0 +1,18 @@
+<?php
+
+$config = [
+    "host"      => "",
+    "port"      => "",
+    "database"  => "",
+    "user"      => "",
+    "password"  => "",
+    "options"   => [
+        PDO::ATTR_TIMEOUT => 5,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::MYSQL_ATTR_FOUND_ROWS   => true,
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+
+    ]
+];

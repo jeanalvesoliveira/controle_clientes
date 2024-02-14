be2c587c6da94ee0dc68dc7a140b63d041254e3d
Jean Alves de Oliveira <jeanalvesoliveira@gmail.com>
2024-02-13 22:06:54 -0300

    Excluindo dados de testes do index.php

--- src/public/index.php
+++ src/public/index.php
@@ -6,34 +6,3 @@ use Jean\ControleClientes\Controllers\ClienteController;
 use Jean\ControleClientes\Models\Cliente;
 
 $controller = new ClienteController();
-
-// $cliente0 = new Cliente(null, "Outro teste doido parte II", "01543279984", "Rua Abdo Zaharam", "31920064545", "alessandrafariacosta@gmail.com");
-// $controller->insert($cliente0);
-
-//$cliente = ClienteController::findById(1);
-
-//var_dump($cliente);
-
-/*
-foreach(ClienteController::findAll() as $cliente){
-    var_dump($cliente);
-}
-*/
-
-// retornando um cliente
-// $cliente = $controller->findById(4);
-// var_dump($cliente);
-
-//fazendo algumas modificações para depois atualizar o registro no banco
-// $cliente->setNome('Jean Alves de Oliveira3');
-// $cliente->setEmail('jeanalvesoliveira@yahoo.com.br');
-
-// echo "novo objeto<br>";
-// var_dump($cliente);
-
-// $controller->update($cliente);
-
-// $cliente2 = $controller->findById(4);
-// var_dump($cliente2);
-
-// $controller->delete(21);

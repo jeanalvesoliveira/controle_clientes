ac529bc229af3662ef13927db6e6bda0859a9ff6
Jean Alves de Oliveira <jeanalvesoliveira@gmail.com>
2024-02-19 17:07:01 -0300

    connect to remote repository

--- src/public/index.php
+++ src/public/index.php
@@ -6,3 +6,7 @@ use Jean\ControleClientes\Controllers\ClienteController;
 use Jean\ControleClientes\Models\Cliente;
 
 $controller = new ClienteController();
+
+$cliente = new Cliente(null, 'Jean', '45645678933', 'Rua tal', '31920052323', 'email@gmail.com');
+
+$controller->insert($cliente);
